690cac73b7273b71720fe970d2c01c5e7943c646
Peyton Zeller-Av <peyton.zeller-av@target.com>
2017-04-18 18:10:10 -0500

    Mocked up some rider constants and binded attributes to html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Rider {
+  id: number;
+  name: string;
+  miles: number;
+};
+
+const RIDERS: Rider[] = [
+  { id: 0, name: 'Henry Winn', miles: 0 },
+  { id: 1, name: 'Peyton Zeller-Av', miles: 0 }
+];
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{title}}</h1>
+              <ul class="riders">
+              <li *ngFor="let rider of allRiders">
+                <span class="badge">{{rider.id}}</span> {{rider.name}} {{rider.miles}}
+              </li>
+             </ul>`
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  title =  'Riders';
+  allRiders = RIDERS;
+}
