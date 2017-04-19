8adad4f753f6d2ed4f17ec533eb18c831a59cdbd
Peyton Zeller-Av <peyton.zeller-av@target.com>
2017-04-19 09:19:01 -0500

    Added styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,57 @@ const RIDERS: Rider[] = [
               <li *ngFor="let rider of allRiders">
                 <span class="badge">{{rider.id}}</span> {{rider.name}} {{rider.miles}}
               </li>
-             </ul>`
+             </ul>`,
+   styles: [`
+     .selected {
+       background-color: #CFD8DC !important;
+       color: white;
+     }
+     .riders {
+       margin: 0 0 2em 0;
+       list-style-type: none;
+       padding: 0;
+       width: 15em;
+     }
+     .riders li {
+       cursor: pointer;
+       position: relative;
+       left: 0;
+       background-color: #EEE;
+       margin: .5em;
+       padding: .3em 0;
+       height: 1.6em;
+       border-radius: 4px;
+     }
+     .riders li.selected:hover {
+       background-color: #BBD8DC !important;
+       color: white;
+     }
+     .riders li:hover {
+       color: #607D8B;
+       background-color: #DDD;
+       left: .1em;
+     }
+     .riders .text {
+       position: relative;
+       top: -3px;
+     }
+     .riders .badge {
+       display: inline-block;
+       font-size: small;
+       color: white;
+       padding: 0.8em 0.7em 0 0.7em;
+       background-color: #607D8B;
+       line-height: 1em;
+       position: relative;
+       left: -1px;
+       top: -4px;
+       height: 1.8em;
+       margin-right: .8em;
+       border-radius: 4px 0 0 4px;
+     }
+   `]
+
 })
 export class AppComponent  {
   title =  'Riders';
